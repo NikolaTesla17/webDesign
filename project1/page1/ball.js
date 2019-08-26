0771f74ef941dd40d3500681df34492d08ed4530
NikolaTesla17 <44534701+NikolaTesla17@users.noreply.github.com>
2019-08-25 23:18:07 -0700

    I hate this branch
    note to future self, pull from master where things arnt like this

--- project1/page1/ball.js
+++ project1/page1/ball.js
@@ -14,22 +14,22 @@ class Ball{
     this.render();
   }
   checkEdges(){//check if touching edge, if so reverse direction
-    // var audio = new Audio('audio_file.mp3');
+    var song = new Audio();
+    song.src = 'audio_file.mp3';
+
     if(this.x<0){
       this.dx = this.dx*-1;//left
-
+      song.play();
     }
     if(this.x>width){
       this.dx = this.dx*-1;//right
-      audio.play();
+      song.play();
     }
     if(this.y<0){
       this.dy = this.dy*-1;//bottom
-      audio.play();
     }
     if(this.y>height){
       this.dy = this.dy*-1;//top
-      playing;
     }
 
 
@@ -41,8 +41,5 @@ class Ball{
   render(){//render one frame
     fill(this.clr);
     ellipse(this.x,this.y,50,50)
-  }
-  playing(){
-    audio.play();
   }
 }
