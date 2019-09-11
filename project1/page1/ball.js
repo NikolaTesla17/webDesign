c26b8c61179969376cd0120f4d916007cc701abc
NikolaTesla17 <44534701+NikolaTesla17@users.noreply.github.com>
2019-09-11 11:08:36 -0700

    donerish with update

--- project1/page1/ball.js
+++ project1/page1/ball.js
@@ -1,35 +1,57 @@
+var p = 0;
 class Ball{
     constructor(x,y,dx,dy){
       this.loc = createVector(x, y);
       this.vel = createVector(dx, dy);
-      this.acc = createVector (0, 0.2);//acceleration vector add
       this.clr = color(random(255),random(255),random(255));
     }
     run(){//use this so you dont have to run all of these for each ball
       this.checkEdges();
       this.update();
       this.render();
+      this.paddle();
+      this.win();
     }
     checkEdges(){//check if touching edge, if so reverse direction
       if(this.loc.x < 0){
+        this.vel.x = -this.vel.x;//left
+      }
+      if(this.loc.x > width){//right
         this.vel.x = -this.vel.x;
       }
-      if(this.loc.x > width){
-        this.vel.x = -this.vel.x;
-      }
-      if(this.loc.y < 0){
+      if(this.loc.y < 0){//down
         this.vel.y = -this.vel.y;
       }
-      if(this.loc.y > height){
+      if(this.loc.y > height){//up
         this.vel.y = -this.vel.y;
+      }
+    }
+    paddle(){
+      var d = mouseY;
+        if((this.loc.y>=(d-50)&&(d+50)>=this.loc.y)&&this.loc.x < 31){//detect paddle hit
+        p++;//incriment score
+        textSize(200);//large score
+        fill(255, 255, 255);//make score text white
+        text(p, 100, 100);//write score
+        this.vel.x = -this.vel.x;//bounce ball
       }
     }
     update(){//update location
-      this.vel.add(this.acc);
-      this.loc.add(this.vel)
+      this.loc.add(this.vel);//move things
+    }
+    win(){
+      if(p>1){
+        background(5,5,255);//wipe background make blue
+        var w = "You Win!";//win string
+        textSize(80);//make text fit
+        fill(255, 255, 255);//make win white
+        text(w, 10, 150);//write you win
+        throw new Error("won");//stop game
+      }
     }
     render(){//render one frame
-      fill(this.clr);
-      ellipse(this.loc.x, this.loc.y, 10, 10);
+      fill(this.clr);//make this color
+      ellipse(this.loc.x, this.loc.y, 10, 10);//render ball, change if you want other shapes
+      background(5,5,5,25);//give slight trail
     }
   }
--- project1/page1/sketch.js
+++ project1/page1/sketch.js
@@ -1,28 +1,18 @@
 //  Nayan Smuek
 // 	8/21/19
-//  Project 8/22
+//  Project 9/11
 
 var balls = [];
-var x = prompt("Number of Balls","42"); //user entered number of balls page dialog
 function setup() {
-  var cnv = createCanvas(500, 400);
-  cnv.position((windowWidth-width)/2, (windowHeight-height)-90);
+  var ctx = createCanvas(400, 400);//make and position canvas
+  ctx.position((windowWidth-width)/2, 175);
   background(5, 5, 5);
   fill(200, 30, 150);
-  for(q=0;q<x;q++){ //create array with number of balls wanted
-    balls[q] = new Ball(random(width),random(height),random(-5,5),random(-5,5))
-  }
+  balls[1] = new Ball(random(width),random(height),random(-5,5),random(-5,5))//make the game ball
+  paddle1 = new Paddle(20,400);//make the paddle
 }
-
 
 function draw() {
-  //for(i=1;i<3;i++){} put other function inside this to run at 60fps
-ballsDraw();
-}
-
-function ballsDraw(){
-  background(5,5,5,50)//get trails back but just a little bit
-  for(i=0;i<x;i++){
-balls[i].run();
-}
+  balls[1].run();//runs the ball
+  paddle1.run();//runs the paddle 
 }
